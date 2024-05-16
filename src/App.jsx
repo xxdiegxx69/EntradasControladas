42c2601ee24a5e837a1eeb22eea7dd4699c144cd
Daniel Ayala <danielayalaof@gmail.com>
2024-05-15 17:33:29 -0700

    hola

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import { useState } from 'react'
+import './App.css';
+
+function App() {
+    const [texto, setTexto] = useState([])
+
+    const handleChange = (event) => {
+      setTexto(event.target.value);
+    }
+
+    return (
+        <div>
+          <div>
+            <input type="text" onChange={handleChange} value={texto} />
+          </div>
+          <h3>Su Texto</h3>
+          <p>
+            {texto}
+          </p>
+        </div>
+    );
+}
+
+export default App;
